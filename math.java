5aec0c6ff07cdf2e911552bc2f2b2a7f6cf5cfc2
unknown <simonastroimanova@hotmail.com>
2023-12-14 18:41:13 +0100

    first java program

---
+++ math.java
@@ -0,0 +1,23 @@
+package zdacaa;
+
+public class math {
+
+
+	public static void main(String[] args) {
+		int x=1, y=1, z=1;
+		if(x>y || z>y) 
+			System.out.print("1");
+	    else 
+			System.out.print("2");
+		if(Math.abs(x-y)>=z)
+		if(x>y)
+		System.out.print("3");
+		else 
+		System.out.print("4");
+		else 
+		System.out.print("5");
+		
+		}
+		
+
+	}
