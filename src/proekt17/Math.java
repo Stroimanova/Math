78cd359853220ea5ded0bc5c67463c739bcb8506
unknown <simonastroimanova@hotmail.com>
2024-01-20 19:25:12 +0100

    first java program

---
+++ src/proekt17/Math.java
@@ -0,0 +1,20 @@
+package proekt17;
+
+public class Math {
+	public static void main(String[] args) {
+		int x=1, y=1, z=1;
+		if(x>y || z>y) 
+			System.out.print("1");
+	    else 
+			System.out.print("2");
+		if(Math.abs(x-y)>=z)
+		if(x>y)
+		System.out.print("3");
+		else 
+		System.out.print("4");
+		else 
+		System.out.print("5");
+		
+		}
+		
+}
